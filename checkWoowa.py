edcf9bfe221fe99ad55576157549379ea80284d7
유윤석 <60517477+rysdid17@users.noreply.github.com>
2021-11-11 00:03:51 +0900

    delete "Nov.09.2021" condition

--- checkWoowa.py
+++ checkWoowa.py
@@ -9,6 +9,3 @@ for link in postLinks:
     if today in link[1]:
         flagWoowa = True
         todayWoowa.append(link[0])
-    if ("Nov.09.2021") in link[1]:
-        flagWoowa = True
-        todayWoowa.append(link[0])
